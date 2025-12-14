8d2a8684898dff9ceb69f78dca72b7cc6fd24c90
ガリレオだけ <151680308+itsjustgalileo@users.noreply.github.com>
2025-12-14 05:04:02 -0500

    Update arena.h

--- arena.h
+++ arena.h
@@ -1,28 +1,28 @@
 #ifndef ARENA_H_
 #define ARENA_H_
 
-    typedef struct Arena Arena;
-    typedef size_t ArenaMarker;
+typedef struct Arena Arena;
+typedef size_t ArenaMarker;
 
-    Arena *arena_init(size_t capacity, const char *name);
-    void arena_free(Arena *arena);
+Arena *arena_init(size_t capacity, const char *name);
+void arena_free(Arena *arena);
 
-    void *arena_malloc(Arena *arena, size_t size);
-    void *arena_aligned_malloc(Arena *arena, size_t size, size_t alignment);
+void *arena_malloc(Arena *arena, size_t size);
+void *arena_aligned_malloc(Arena *arena, size_t size, size_t alignment);
 
-    void arena_reset(Arena *arena);
-    int arena_get_size(Arena *arena, size_t *size);
-    int arena_get_available(Arena *arena, size_t *size);
-    int arena_get_capacity(Arena *arena, size_t *capacity);
-    int arena_top(Arena *arena, void **top);
+void arena_reset(Arena *arena);
+int arena_get_size(Arena *arena, size_t *size);
+int arena_get_available(Arena *arena, size_t *size);
+int arena_get_capacity(Arena *arena, size_t *capacity);
+int arena_top(Arena *arena, void **top);
 #ifndef NDEBUG
-    const char *arena_get_name(Arena *arena);
-    void arena_print(Arena *arena);
+const char *arena_get_name(Arena *arena);
+void arena_print(Arena *arena);
 #endif /* NDEBUG */
     
-    ArenaMarker arena_mark(Arena *arena);
-    void arena_rewind(Arena *arena, ArenaMarker marker);
+ArenaMarker arena_mark(Arena *arena);
+void arena_rewind(Arena *arena, ArenaMarker marker);
 
-    Arena *arena_create_subarena(Arena *parent, size_t capacity, const char *name);
+Arena *arena_create_subarena(Arena *parent, size_t capacity, const char *name);
 
 #endif /* ARENA_H_ */
