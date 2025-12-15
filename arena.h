894fec6dbddaa758ebcdf059684ca0e5724e70db
ガリレオだけ <151680308+itsjustgalileo@users.noreply.github.com>
2025-12-15 12:52:11 -0500

    Update arena.h

--- arena.h
+++ arena.h
@@ -1,23 +1,25 @@
 #ifndef ARENA_H_
 #define ARENA_H_
 
+#include <stdlib.h>
+
 typedef struct Arena Arena;
 typedef size_t ArenaMarker;
 
 Arena *arena_init(size_t capacity, const char *name);
 void arena_free(Arena *arena);
+void arena_reset(Arena *arena);
 
 void *arena_malloc(Arena *arena, size_t size);
 void *arena_aligned_malloc(Arena *arena, size_t size, size_t alignment);
 
-void arena_reset(Arena *arena);
-int arena_get_size(Arena *arena, size_t *size);
-int arena_get_available(Arena *arena, size_t *size);
-int arena_get_capacity(Arena *arena, size_t *capacity);
-int arena_top(Arena *arena, void **top);
+size_t arena_get_size(const Arena *arena);
+size_t arena_get_available(const Arena *arena);
+size_t arena_get_capacity(const Arena *arena);
+void *arena_top(const Arena *arena);
 #ifndef NDEBUG
-const char *arena_get_name(Arena *arena);
-void arena_print(Arena *arena);
+const char *arena_get_name(const Arena *arena);
+void arena_print(const Arena *arena);
 #endif /* NDEBUG */
     
 ArenaMarker arena_mark(Arena *arena);
