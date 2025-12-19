7267a11af1c4f0db58e4d00f0edb8ea18283bf0a
ItsJustGalileo <itsjustgalileo@icloud.com>
2025-12-18 23:50:48 -0500

    standalone v1.2

--- arena.c
+++ arena.c
@@ -1,48 +1,66 @@
 #include <stdio.h>
-#include <stddef.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <assert.h>
 
 #include "./arena.h"
 
 #define MAX_ARENA_COUNT 256
+#define MAX_SUBARENA_COUNT (MAX_ARENA_COUNT / 2)
 
 static unsigned int arena_count;
+static unsigned int subarena_count;
 
 struct Arena {
     size_t capacity; /* Arena's total capacity */
-    size_t used; /* Arena's used size */
+    size_t offset; /* Arena's offset size */
     void *base; /* Arena's starting point */
     bool is_sub; /* Helps with free */
 #ifndef NDEBUG
-    const char debug_name[256]; /* if we want to easily track them */
+    char debug_name[256]; /* if we want to easily track them */
 #endif /* NDEBUG */
 };
 
 Arena *arena_init(size_t capacity, const char *name) {
+    assert(arena_count <= MAX_ARENA_COUNT);
+    assert(strlen(name) + 1 <= 256 && "Arena's name is too long");
+
     Arena *arena = (Arena *)malloc(sizeof(Arena));
     if (NULL == arena) {
         fprintf(stderr, "[arena_init]: Failed to allocate memory Arena\n");
         return NULL;
     }
 
+    if (capacity == 0) {
+        ++capacity;
+    }
+    
     arena->is_sub = false;
     arena->capacity = capacity;
-    arena->used = 0;
+    arena->offset = 0;
+
 #ifndef NDEBUG
-    if (name) {        
-        arena->debug_name = name;
+    if (name != NULL) {
+        snprintf(arena->debug_name, sizeof(arena->debug_name), "%s", name);
     } else {
-        snprintf(arena->debug_name, sizeof("unnamed_arenaXXX"), "%s%03d", "unnamed_arena", arena_count++);        
+        snprintf(arena->debug_name, sizeof("unnamed_arenaXXX"), "%s%03u", "unnamed_arena", arena_count);
     }
 #endif
-    
-    arena->base = malloc(capacity);
+
+    arena->base = malloc(arena->capacity);
     if (NULL == arena->base) {
         fprintf(stderr, "[arena_init]: Failed to allocate memory for base\n");
         free(arena);
         return NULL;
     }
-    
+
+#ifndef NDEBUG
+    memset(arena->base, 0xEF, capacity);
+#endif /* NDEBUG */
+    ++arena_count;
     return arena;
 }
 
@@ -59,68 +77,85 @@ void arena_free(Arena *arena) {
     }
     free(arena->base);
     free(arena);
+    --arena_count;
     return;
 }
 
 void *arena_malloc(Arena *arena, size_t size) {
     assert(arena);
-    
-    if (0 == size) {
+
+    if (size == 0) {
         ++size;
     }
-    
-    if (arena->used + size > arena->capacity) {
-        fprintf(stderr, "[arena_malloc]: Buffer overrun\n");
+
+    const size_t alignment = _Alignof(max_align_t);
+    uintptr_t current = (uintptr_t)arena->base + arena->offset;
+    uintptr_t aligned = (current + (alignment - 1)) & ~(alignment - 1);
+    size_t padding = aligned - current;
+
+    if (arena->offset + size + padding > arena->capacity) {
+        fprintf(stderr, "[arena_aligned_malloc]: Buffer overrun\n");
         return NULL;
     }
-    void *ptr = (char *)arena->base + arena->used;
-    arena->used += size;
+
+    void *ptr = (void *)aligned;
+    arena->offset += size + padding;
+
 #ifndef NDEBUG
     memset(ptr, 0xAD, size);
 #endif /* NDEBUG */
-    
+
     return ptr;
 }
 
 void *arena_aligned_malloc(Arena *arena, size_t size, size_t alignment) {
     assert(arena);
-    if (0 == size) {
-        ++size;
-    }
 
+    /* Making sure alignment is a power of two */
     assert((alignment & (alignment - 1)) == 0);
 
-    uintptr_t current = (uintptr_t)arena->base + arena->used;
+    if (size == 0) {
+        ++size;
+    }
+    
+    uintptr_t current = (uintptr_t)arena->base + arena->offset;
     uintptr_t aligned = (current + (alignment - 1)) & ~(alignment - 1);
     size_t padding = aligned - current;
 
-    if (arena->used + size + padding > arena->capacity) {
+    if (arena->offset + size + padding > arena->capacity) {
         fprintf(stderr, "[arena_aligned_malloc]: Buffer overrun\n");
         return NULL;
     }
-    
+
     void *ptr = (void *)aligned;
-    arena->used += size + padding;
+    arena->offset += size + padding;
+
+#ifndef NDEBUG
+    memset(ptr, 0xAD, size);
+#endif /* NDEBUG */
+
     return ptr;
 }
 
 void arena_reset(Arena *arena) {
     assert(arena);
+
 #ifndef NDEBUG
-    my_memset(arena->base, 0xDE, arena->used);
+    memset(arena->base, 0xDE, arena->offset);
 #endif /* NDEBUG */
-    arena->used = 0;
+
+    arena->offset = 0;
     return;
 }
 
 size_t arena_get_size(const Arena *arena) {
     assert(arena);
-    return arena->used;
+    return arena->offset;
 }
 
 size_t arena_get_available(const Arena *arena) {
     assert(arena);
-    return arena->capacity - arena->used;
+    return arena->capacity - arena->offset;
 }
 
 size_t arena_get_capacity(const Arena *arena) {
@@ -128,65 +163,85 @@ size_t arena_get_capacity(const Arena *arena) {
     return arena->capacity;
 }
 #ifndef NDEBUG
-const char *arena_get_name(Arena *arena) {
+const char *arena_get_name(const Arena *arena) {
     assert(arena);
     return arena->debug_name;
 }
 
-void arena_print(Arena *arena) {
+void arena_print(const Arena *arena) {
     assert(arena);
     fprintf(stderr, "[Arena: %s] Used: %zu / %zu bytes\n",
-            arena->debug_name ? arena->debug_name : "(unnamed)",
-            arena->used, arena->capacity);
+            arena->debug_name,
+            arena->offset, arena->capacity);
     return;
 }
 #endif /* NDEBUG */
 
 void *arena_top(const Arena *arena) {
-    assert(arena && top);
-    return (char *)arena->base + arena->used;
+    assert(arena);
+    return (char *)arena->base + arena->offset;
+}
+
+bool arena_is_sub(const Arena *arena) {
+    assert(arena);
+    return arena->is_sub;
 }
 
 ArenaMarker arena_mark(const Arena *arena) {
     assert(arena);
-    return arena->used;
+    return arena->offset;
 }
 
 void arena_rewind(Arena *arena, const ArenaMarker marker) {
     assert(arena);
-    if (marker > arena->used || marker > arena->capacity) {
+
+    if (marker > arena->offset || marker > arena->capacity) {
         fprintf(stderr, "[arena_rewind]: Invalid rewind marker: out of bounds\n");
         return;
     }
-    arena->used = marker;
+
+    arena->offset = marker;
     return;
 }
 
-Arena *arena_create_subarena(const Arena *parent, size_t capacity, const char *name) {
-    assert(parent);
-    if (0 == capacity) {
-        ++capacity;
-    }
+Arena *arena_create_subarena(Arena *parent, size_t capacity, const char *name) {
+    assert(parent && capacity);
+    assert(subarena_count <= MAX_SUBARENA_COUNT);
+    assert(strlen(name) + 1 <= 256 && "Subarena's name is too long");
+    
+    // Keeping this to rollback if we fail to allocate the subarena's base
+    ArenaMarker rollback = arena_mark(parent);
+
     // using _Alignof is not optimal. Maybe we should create cross-platform macro.
-    Arena *subarena = (Arena *)arena_aligned_malloc(parent, sizeof(Arena), _Alignof(Arena));
+    Arena *subarena = (Arena *)arena_aligned_malloc(parent, sizeof(Arena), _Alignof(max_align_t));
     if (NULL == subarena) {
         fprintf(stderr, "[arena_create_subarena]: Failed to malloc subarena\n");
         return NULL;
     }
     subarena->is_sub = true;
-#ifndef NDEBUG
-    subarena->debug_name = name ? name : "anonymous_subarena";
-#endif /* NDEBUG */
+
     
+#ifndef NDEBUG
+    if (name != NULL) {
+        snprintf(subarena->debug_name, sizeof(subarena->debug_name), "%s", name);
+    } else {
+        snprintf(subarena->debug_name, sizeof("unnamed_subarenaXXX"), "%s%03u", "unnamed_subarena", subarena_count);
+    }
+#endif /* NDEBUG */
+
     void *sub_base = arena_aligned_malloc(parent, capacity, _Alignof(max_align_t));
     if (NULL == sub_base) {
+        /* rollback parent by capacity */
+        arena_rewind(parent, rollback);
         fprintf(stderr, "[arena_create_subarena]: Failed to malloc sub base\n");
         return NULL;
     }
 
     subarena->base = sub_base;
-    subarena->used = 0;
+    subarena->offset = 0;
     subarena->capacity = capacity;
+
+    ++subarena_count;
     
     return subarena;
 }
--- arena.h
+++ arena.h
@@ -1,30 +1,171 @@
+/**
+ * @file arena.h
+ * @author itsjustgalileo
+ * @version 1.2
+ * @brief Basic bump allocator in C.
+ */
 #ifndef ARENA_H_
 #define ARENA_H_
 
 #include <stdlib.h>
+#include <stdbool.h>
 
-typedef struct Arena Arena;
-typedef size_t ArenaMarker;
+/* Set up for C function definitions, even when using C++ */
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-Arena *arena_init(size_t capacity, const char *name);
-void arena_free(Arena *arena);
-void arena_reset(Arena *arena);
+    /**
+     * @brief An opaque type representing the arena type.
+     */
+    typedef struct Arena Arena;
 
-void *arena_malloc(Arena *arena, size_t size);
-void *arena_aligned_malloc(Arena *arena, size_t size, size_t alignment);
+    /**
+     * @brief An unsigned integer to hold the return
+     * pseudo-address for arena_rewind(). Remains valid
+     * only as long as the arena's state remains unchanged.
+     */
+    typedef size_t ArenaMarker;
 
-size_t arena_get_size(const Arena *arena);
-size_t arena_get_available(const Arena *arena);
-size_t arena_get_capacity(const Arena *arena);
-void *arena_top(const Arena *arena);
-#ifndef NDEBUG
-const char *arena_get_name(const Arena *arena);
-void arena_print(const Arena *arena);
-#endif /* NDEBUG */
+    /**
+     * @brief Creates an arena with a given non-zero capacity.
+     *
+     * If capacity == 0, a minimum allocation of 1 byte is performed.
+     *
+     * @param capacity The size of the chunk of memory to allocate for the entire pool.
+     * @param name (optional) A name for easier debug tracking.
+     *
+     * @return A pointer to the arena object if success, or NULL if failure.
+     */
+    extern Arena *arena_init(size_t capacity, const char *name);
+
+    /**
+     * @brief Frees an entire arena. Never call this on a subarena.
+     *
+     * @param arena A pointer to the arena to free.
+     */
+    extern void arena_free(Arena *arena);
+
+    /**
+     * @brief Rewinds an arena back to the base. This does not free
+     * any allocated memory.
+     *
+     * All previously returned pointers become invalid.
+     */
+    extern void arena_reset(Arena *arena);
+
+    /**
+     * @brief Returns a pointer to the allocated memory and bumps
+     * the allocator's offset by the requested non-zero size, aligned to
+     * the alignment of max_align_t.
+     *
+     * If size == 0, a minimum allocation of 1 byte is performed.
+     *
+     * @param arena The arena pool in which to allocate.
+     * @param size The size of the desired memory chunk.
+     * 
+     * @return A pointer to the allocated memory chunk.
+     */
+    extern void *arena_malloc(Arena *arena, size_t size);
+
+    /**
+     * @brief Returns a pointer to the allocated memory and bumps
+     * the allocator's offset by the requested size, aligned to
+     * the requested size.
+     *
+     * If size == 0, a minimum allocation of 1 byte is performed.
+     *
+     * @param arena A pointer to the arena in which to allocate.
+     * @param size The size of the desired memory chunk.
+     * @param alignment The size of memory alignment. Must be > 0 and a power of 2.
+     *
+     * @return A pointer to the allocated memory chunk.
+     */
+    extern void *arena_aligned_malloc(Arena *arena, size_t size, size_t alignment);
+
+    /**
+     * @brief Get the size of allocated memory in a given arena.
+     *
+     * @param arena A pointer to the arena object.
+     *
+     * @return The offset of the arena.
+     */
+    extern size_t arena_get_size(const Arena *arena);
+
+    /**
+     * @brief Get the remaining memory in bytes in a given arena
+     *
+     * @param arena A pointer to the arena object.
+     *
+     * @return The available size in the arena.
+     */
+    extern size_t arena_get_available(const Arena *arena);
     
-ArenaMarker arena_mark(const Arena *arena);
-void arena_rewind(Arena *arena, const ArenaMarker marker);
+    /**
+     * @brief Get the total capacity of a given arena.
+     *
+     * @param arena A pointer to the arena object.
+     *
+     * @return The capacity of the arena.
+     */
+    extern size_t arena_get_capacity(const Arena *arena);
 
-Arena *arena_create_subarena(const Arena *parent, size_t capacity, const char *name);
+    /**
+     * @brief Return the new base of the arena after allocations.
+     *
+     * @param arena A pointer to the arena object.
+     *
+     * @return A pointer to the next available memory address.
+     */
+    extern void *arena_top(const Arena *arena);
+
+#ifndef NDEBUG
+    extern const char *arena_get_name(const Arena *arena);
+    extern void arena_print(const Arena *arena);
+#endif /* NDEBUG */
+
+    /**
+     * @brief Check if a given arena is a subarena or a parent.
+     *
+     * @param arena The arena to check.
+     *
+     * @return true if the arena is a subarena, false if not.
+     */
+    extern bool arena_is_sub(const Arena *arena);
+
+    /**
+     * @brief Set a checkpoint in memory.
+     *
+     * @param arena The arena in which to set the marker.
+     *
+     * return An integer (pseudo-address) that acts as a marker.
+     */
+    extern ArenaMarker arena_mark(const Arena *arena);
+
+    /**
+     * @brief Rewinds an arena back to a given marker.
+     *
+     * @param arena The arena to rewind.
+     * @param marker The marker to jump back to.
+     */
+    extern void arena_rewind(Arena *arena, const ArenaMarker marker);
+
+    /**
+     * @brief Creates a secondary arena that branches off of an already existing one.
+     *
+     * Subarenas do not own memory and are invalidated if the parent is rewound past their creation point.
+     *
+     * If capacity == 0, a minimum bump of one byte is performed.
+     *
+     * @param parent The arena to branch from.
+     * @param capacity The total size of the arena.
+     * @param name (optional) debug name.
+     */
+    extern Arena *arena_create_subarena(Arena *parent, size_t capacity, const char *name);
+
+/* Ends C function definitions when using C++ */
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* ARENA_H_ */
